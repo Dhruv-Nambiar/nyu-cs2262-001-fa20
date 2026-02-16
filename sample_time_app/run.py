f38ffb47f6fac34e6f443080c83ad3ebf699e74c
Dhruv Nambiar <dhruvnambiar1@gmail.com>
2026-02-15 19:40:39 -0500

    Update python version in docker img, added /time

--- sample_time_app/run.py
+++ sample_time_app/run.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from datetime import datetime
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello world!'
+
+@app.route('/time')
+def print_time():
+    return "Time: " + datetime.now().strftime("%H:%M:%S") + " UTC"
 
 
 app.run(host='0.0.0.0',
